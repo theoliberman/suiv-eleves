5fa5675bfebdafabbed6dae99f9da9464fe0323b
Theo Liberman <liberman.theo@gmail.com>
2025-09-13 12:36:42 +0200

    Design dashboard layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  modules: ["@nuxt/ui"],
+
+  css: ["~/assets/css/main.css"],
+
+  appConfig: {
+    ui: {
+      colors: {
+        primary: "emerald",
+        neutral: "neutral",
+      },
+    },
+  },
 });
